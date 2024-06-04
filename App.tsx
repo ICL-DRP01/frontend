a255c2ae80c201e04f44987870ef8dacd3c8f29c
harens <12570877+harens@users.noreply.github.com>
2024-06-04 16:57:13 +0100

    test1

--- App.tsx
+++ App.tsx
@@ -1,18 +1,96 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Reserve from './src/screens/Reserve';
+import { StatusBar } from "expo-status-bar";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
 
-const Stack = createStackNavigator();
+// notifications imports
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants"; // Optional
 
-const App = () => {
+// Initialize the notification service
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+// Register for push notifications [TODO: You can move this to a separate file]
+export async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    // Don't forget to import Platform from react-native
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+
+    if (Constants.easConfig?.projectId) {
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId: Constants.easConfig.projectId, // you can hard code project id if you dont want to use expo Constants
+        })
+      ).data;
+      console.log(token);
+    }
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  return token;
+}
+
+// Send the notification [TODO: You can move this to a separate file or export it as a function to be called from anywhere]
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got notification! 🔔",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 2 }, // You can change this to any time interval you want
+  });
+}
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Reserve" component={Reserve} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* Test button to schedule notifications */}
+      <Button
+        title="Send Notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
